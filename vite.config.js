c3db81c0d79116d2b35115ca3967be0f052ab6ea
ashtyngon <ezrikson@gmail.com>
2025-09-30 11:13:43 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/zohran-canvassing-guide/', // <-- important for GitHub Project Pages
+  base: '/zohran-canvassing-guide/',
+  plugins: [react()]
 })
